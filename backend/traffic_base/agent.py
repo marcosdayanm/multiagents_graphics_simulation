7c77c238b955f032e4e14233743cd22792ee2401
zuamaya <a01784238@tec.mx>
2024-11-25 15:56:13 -0600

    indirect merge

--- backend/traffic_base/agent.py
+++ backend/traffic_base/agent.py
@@ -1,35 +1,105 @@
-import mesa
+from mesa import Agent
+from collections import deque
 
-class Car(mesa.Agent):
-    def __init__(self, unique_id, model, status: str, route: list[tuple[tuple[int],tuple[int]]], destination_coords: tuple[int]):
+class Car(Agent):
+    def __init__(self, unique_id, model, status: str, street_graph: dict, destination_coords: list[tuple[int, int]]): # route: list[tuple[tuple[int, int],tuple[int, int]]]
         super().__init__(unique_id, model)
         self.status = status # calculating_route, following_route, arrived
-        self.route = route
+        self.route = []
         self.destination_coords = destination_coords
         self.color = "blue"
+        self.street_graph = street_graph
+        self.direction = (0, 0)
 
 
-    def pathfinding_heuristic(self, a, b):
+    def bfs(self, graph: dict[tuple[int, int], list[tuple[int, int]]], start: tuple[int, int], goal: tuple[int, int]) -> list[tuple[tuple[int, int], tuple[int, int]]]:
         """
-        Method to calculate the euclidean distance to compare between its options as an heuristic for the pathfinding algorithm
+        This method finds the shortest path between two points in a graph using the Breadth First Search algorithm.
+        It starts from the goal coordinate, expanding all its neighbors and saving the path on the "parent" dictionary.
         """
-        return abs(a[0] - b[0]) + abs(a[1] - b[1])
+        queue = deque()
+        queue.append(start)
+        visited = set()
+        visited.add(start)
+        parent = {}
+        
+        while queue:
+            current = queue.popleft()
+            if current == goal:
+                # Rebuild the path once the goal is reached
+                path = []
+                while current != start:
+                    path.append(current)
+                    current = parent[current]
+                path.reverse()
 
-    def calculate_route(self):
-        all_neighbors = self.model.grid.get_neighborhood(
-            self.pos,
-            moore=True,
-            include_center=False
-        )
+                # Adding the direction chance for car representation in WebGl
+                path_with_directions = []
+                previous = start
+                for coord in path:
+                    direction = self.get_direction(previous, coord)
+                    path_with_directions.append((coord, direction))
+                    previous = coord
+                return path_with_directions
 
-    def move(self):     
-        self.model.grid.move_to_empty(self)
+            for neighbor in graph.get(current, []):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    parent[neighbor] = current
+                    queue.append(neighbor)
+        return None  # Path not found
+    
+
+    def get_direction(self, from_coord: tuple[int, int], to_coord: tuple[int, int]) -> tuple[int, int]:
+        """
+        Determines the direction between two adjacent coordinates.
+        Returns 'L', 'R', 'U', or 'D' for left, right, up, or down respectively.
+        """
+        dx = to_coord[0] - from_coord[0]
+        dy = to_coord[1] - from_coord[1]
+        
+        if dx == 1 and dy == 0:
+            return (0,1) 
+        elif dx == -1 and dy == 0:
+            return (0,-1) 
+        elif dx == 0 and dy == 1:
+            return (1,0) 
+        elif dx == 0 and dy == -1:
+            return (-1,0)
+
+
+    def subsumption(self):
+        if self.status == "calculating_route": # Calculating route
+            self.route = self.bfs(self.street_graph, self.pos, self.random.choice(self.destination_coords))
+            if self.route:
+                self.status = "following_route"
+                self.model.average_steps_to_destination = self.model.average_steps_to_destination + ((len(self.route) - self.model.average_steps_to_destination) / (self.model.total_car_number + 1))
+
+        elif self.status == "following_route": # While following route
+            if self.route:
+                next_cell_contents = self.model.grid.get_cell_list_contents(self.route[0][0])
+                is_car_agent = len([agent for agent in next_cell_contents if isinstance(agent, Car)]) > 0
+                is_red_traffic_light = len([agent for agent in next_cell_contents if isinstance(agent, Traffic_Light) and agent.is_red]) > 0
+                if is_car_agent or is_red_traffic_light:
+                    # TODO: Implement way of checking the other lane while in traffic of in a red light
+                    return 
+                pos_to_move, direction = self.route.pop(0)
+                self.model.grid.move_agent(self, pos_to_move)
+                self.direction = direction
+
+            else:
+                self.status = "arrived"
+                self.color = "white"
+                self.model.schedule.remove(self)
+                self.model.grid.remove_agent(self)
+                self.model.total_cars_at_destination += 1
+                self.model.car_number -= 1
 
     def step(self):
-        self.move()
+        self.subsumption()
 
 # Delete the destination atributte from the class Road, it's neccesary a new class Destination
-class Road(mesa.Agent):
+class Road(Agent):
     def __init__(self, unique_id, model, directions: list[str]):
         super().__init__(unique_id, model)
         self.directions = directions
@@ -38,7 +108,7 @@ class Road(mesa.Agent):
     def step(self):
         pass
 
-class Traffic_Light(mesa.Agent):
+class Traffic_Light(Agent):
     def __init__(self, unique_id, model, directions: list[str], is_red: bool, time_to_change: int, color: str):
         super().__init__(unique_id, model)
         self.directions = directions
@@ -63,7 +133,7 @@ class Traffic_Light(mesa.Agent):
         
         self.time_to_change -= 1
 
-class Obstacle(mesa.Agent):
+class Obstacle(Agent):
     """
     Obstacle agent. Just to add obstacles to the grid.
     """
@@ -74,7 +144,7 @@ class Obstacle(mesa.Agent):
     def step(self):
         pass
 
-class Destination(mesa.Agent):
+class Destination(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.color = "orange"
--- backend/traffic_base/model.py
+++ backend/traffic_base/model.py
@@ -1,31 +1,38 @@
 # Model class for the traffic simulation
-import mesa
+from mesa import Agent 
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from traffic_base.read_map import build_graph
 from traffic_base.agent import Road, Traffic_Light, Obstacle, Destination
 import json
 
-class CityModel(mesa.Model):
-    def __init__(self, N):
-        super().__init__()
 
-        print("Init model called")
+class CityModel(Model):
+    def __init__(self, N, place_cars_interval: int = 5):
 
-        street_graph, grid, grid_info = build_graph('map_files/2021_base.txt')
+        street_graph, grid, grid_info = build_graph('city_files/2022_base.txt')
 
-        self.width = int(len(grid[0]))
-        self.height = int(len(grid))
-        self.grid = MultiGrid(self.width, self.height, torus=False) 
+        self.width = len(grid[0])
+        self.height = len(grid)
+        self.grid = MultiGrid(self.width, self.height, torus = False) 
         self.schedule = RandomActivation(self)
-        self.grid_info = grid_info  # Store grid_info as an attribute
 
-        self.id_counter = 0  # Contador de IDs únicas
+        self.place_cars_interval = place_cars_interval
+        self.grid_info = grid_info
+        self.street_graph = street_graph
+
+        # Stats
+        self.total_cars_at_destination = 0
+        self.total_car_number = 0
+        self.car_number = 0
+        self.average_steps_to_destination = 0
+
 
         for y in range(self.height):
             for x in range(self.width):
                 symbol = grid[y][x]
-                self.placeAgent((x, y), symbol)  # Use the new method
+                mesa_y = self.height - 1 - y
+                self.place_env_agent((x, mesa_y), symbol)
 
         self.num_agents = N
         self.running = True
@@ -39,160 +46,53 @@ class CityModel(mesa.Model):
 
     def step(self):
         '''Advance the model by one step.'''
+        if self.schedule.steps % self.place_cars_interval == 0:
+            self.place_cars()
         self.schedule.step()
-    
-    def placeAgent(self, pos: tuple[int, int], symbol: str):
+
+
+    def place_cars(self):
         """
-        Create and place an agent in the grid based on the given symbol.
-
-        Args:
-            pos (tuple[int, int]): Coordinates (x, y) in the grid.
-            symbol (str): Character representing the type of agent.
+        Place cars in each corner of the grid
         """
-        unique_id = f"{symbol}_{self.id_counter}"  # Generar ID única
-        self.id_counter += 1  # Incrementar el contador de IDs
-
-        if symbol in ["v", "^", ">", "<"]:
-            agent = Road(unique_id, self, [symbol])
-        
-        elif symbol in ["S", "s"]:
-            is_red = False if symbol == "S" else True
-            agent = Traffic_Light(unique_id, self, [symbol], is_red, 10, "red" if is_red else "green")
-
-        elif symbol == "#":
-            agent = Obstacle(unique_id, self)
-
-        elif symbol == "D":
-            agent = Destination(unique_id, self)
-            self.grid_info["destinations"].append(pos)
-
-        else:
-            print(f"Warning: Unrecognized symbol '{symbol}' at position {pos}")
-            return
-
-        self.grid.place_agent(agent, pos)
-        self.schedule.add(agent)
-        
-
-# class CityModel(mesa.Model):
-#     def __init__(self, N):
-#         super().__init__()
-        
-#         print("Init model called")
-
-#         street_graph, grid, grid_info = build_graph('map_files/2022_base.txt')
-
-#         self.width = int(len(grid[0]))
-#         self.height = int(len(grid))
-#         self.grid = MultiGrid(self.width, self.height, torus=False) 
-#         self.schedule = RandomActivation(self)
-#         self.grid_info = grid_info  # Store grid_info as an attribute
-
-#         for y in range(self.height):
-#             for x in range(self.width):
-#                 symbol = grid[y][x]
-#                 self.placeAgent((x, y), symbol)  # Use the new method
-
-#         self.num_agents = N
-#         self.running = True
-        
-#         print("Init model finished")
-
-#     def step(self):
-#         '''Advance the model by one step.'''
-#         self.schedule.step()
-
-#     def placeAgent(self, pos: tuple[int, int], symbol: str):
-#         """
-#         Create and place an agent in the grid based on the given symbol.
-
-#         Args:
-#             pos (tuple[int, int]): Coordinates (x, y) in the grid.
-#             symbol (str): Character representing the type of agent.
-#         """
-#         if symbol in ["v", "^", ">", "<"]:
-#             agent = Road(f"r_{pos[0] * self.width + pos[1]}", self, [symbol])
-        
-#         elif symbol in ["S", "s"]:
-#             is_red = False if symbol == "S" else True
-#             agent = Traffic_Light(f"tl_{pos[0] * self.width + pos[1]}", self, [symbol], is_red, 10, "red" if is_red else "green")
-
-#         elif symbol == "#":
-#             agent = Obstacle(f"ob_{pos[0] * self.width + pos[1]}", self)
-
-#         elif symbol == "D":
-#             agent = Destination(f"d_{pos[0] * self.width + pos[1]}", self)
-#             self.grid_info["destinations"].append(pos)
-
-#         else:
-#             print(f"Warning: Unrecognized symbol '{symbol}' at position {pos}")
-#             return
-        
-#         # Cuando agregues agentes a la grilla
-#         # print(f"Placing agent: {type(agent)} at position {pos}")
-#         self.grid.place_agent(agent, pos)
-#         # print(f"Agent of type {type(agent).__name__} placed at {pos}")
-#         self.schedule.add(agent)
-        
-# from mesa import Model
-# from mesa.time import RandomActivation
-# from mesa.space import MultiGrid
-# from traffic_base.read_map import build_graph
-# from traffic_base.agent import *
-# import json
-
-
-# class CityModel(Model):
-#     def __init__(self, N):
-        
-#         super().__init__()
-        
-#         print("Init model called")
-
-#         street_graph, grid, grid_info = build_graph('../map_files/2022_base.txt')
-
-#         self.width = len(grid[0])
-#         self.height = len(grid)
-#         self.grid = MultiGrid(self.width, self.height, torus = False) 
-#         self.schedule = RandomActivation(self)
-
-#         for y in range(self.height):
-#             for x in range(self.width):
-#                 symbol = grid[y][x]
-#                 place_agent((x, y), symbol, self, grid_info)
-
-#         self.num_agents = N
-#         self.running = True
-        
-#         print("Init model finished")
-
-#     def step(self):
-#         '''Advance the model by one step.'''
-#         self.schedule.step()
+        for pos in [(0,0), (0, self.height - 1), (self.width - 1, 0), (self.width - 1, self.height - 1)]: # grid corners
+            car = Car(f"c_{pos[0] * self.width + pos[1]}", self, "calculating_route", self.street_graph, self.grid_info["destinations"])
+            self.grid.place_agent(car, pos)
+            self.schedule.add(car)
+            
+            self.total_car_number += 1
+            self.car_number += 1
 
 
 
-# def place_agent(pos: tuple[int, int], symbol: str, model: CityModel, grid_info: dict):
-#     """
-    
-#     """
-#     if symbol in ["v", "^", ">", "<"]:
-#         agent = Road(f"r_{pos[0] * model.width + pos[1]}", model, [symbol])
-        
-#     elif symbol in ["S", "s"]:
-#         is_red = False if symbol == "S" else True
-#         agent = Traffic_Light(f"tl_{pos[0] * model.width + pos[1]}", model, [symbol], is_red, 10, "red" if is_red else "green")
+    def place_env_agent(self, pos: tuple[int, int], symbol: str):
+            """
+            Create and place an agent in the grid based on the given symbol.
 
-#     elif symbol == "#":
-#         agent = Obstacle(f"ob_{pos[0] * model.width + pos[1]}", model)
+            Args:
+                pos (tuple[int, int]): Coordinates (x, y) in the grid.
+                symbol (str): Character representing the type of agent.
+            """
+            unique_id = f"{symbol}_{self.id_counter}"  # Generar ID única
+            self.id_counter += 1  # Incrementar el contador de IDs
 
-#     elif symbol == "D":
-#         agent = Destination(f"d_{pos[0] * model.width + pos[1]}", model)
-#         grid_info["destinations"].append(pos)
+            if symbol in ["v", "^", ">", "<"]:
+                agent = Road(unique_id, self, [symbol])
+            
+            elif symbol in ["S", "s"]:
+                is_red = False if symbol == "S" else True
+                agent = Traffic_Light(unique_id, self, [symbol], is_red, 10, "red" if is_red else "green")
 
-#     else:
-#         return
+            elif symbol == "#":
+                agent = Obstacle(unique_id, self)
 
-#     model.grid.place_agent(agent, pos)
-#     print("Agent of type", type(agent), "placed at", pos)
-#     model.schedule.add(agent)
+            elif symbol == "D":
+                agent = Destination(unique_id, self)
+                self.grid_info["destinations"].append(pos)
+
+            else:
+                print(f"Warning: Unrecognized symbol '{symbol}' at position {pos}")
+                return
+
+            self.grid.place_agent(agent, pos)
+            self.schedule.add(agent)
